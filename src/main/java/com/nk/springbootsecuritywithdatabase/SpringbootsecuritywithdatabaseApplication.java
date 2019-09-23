e1ff0cb108f22cc50dcaff6213046befc15d1357
King <nigus.kidane@gmail.com>
2019-09-22 21:59:04 -0600

    Project created

---
+++ src/main/java/com/nk/springbootsecuritywithdatabase/SpringbootsecuritywithdatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.nk.springbootsecuritywithdatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootsecuritywithdatabaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootsecuritywithdatabaseApplication.class, args);
+    }
+
+}
